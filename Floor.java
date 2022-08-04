9d08f26870afda956c84d4369684479d426ffa81
NAYANQA <109003123+NAYANQA@users.noreply.github.com>
2022-08-04 01:13:52 +0100

    Add files via upload
    Home work Week08

---
+++ Floor.java
@@ -0,0 +1,27 @@
+package carpetcostcalculater;
+
+public class Floor {
+
+    private double width;
+    private double lenght;
+
+    public Floor(double width, double lenght){
+        if (width < 0 && lenght >0){
+            this.width = 0;
+            this.lenght = lenght;
+        } else if (width >0 && lenght <0) {
+            this.width = width;
+            this.lenght = 0;
+        } else if (width< 0 && lenght< 0) {
+            this.width = 0;
+            this.lenght = 0;
+        }
+        else {
+            this.width = width;
+            this.lenght = lenght;
+        }
+    }
+public double getArea(){
+        return this.width * this.lenght;
+}
+}
